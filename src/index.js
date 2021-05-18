ee387e0027b0450a7f1eacf6044b163949102fc1
mituoka <mitutumi0210@gmail.com>
2021-05-18 23:47:46 +0900

    js xs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import "./styles.css";
+// // テンプレート文字列
+// const name = "光岡";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // 従来の方法
+// const message1 = "私は" + name;
+// // テンプレート構文
+// console.log(message1);
+// const message2 = `私は${name}`;
+
+// // アロー関数
+// function func1(str) {
+//   return str;
+// }
+
+// console.log(func1("func1です"));
+
+// const func2 = (str) => {
+//   return str;
+// };
+
+// console.log(func2("func2です"));
+
+// const numArray = [1, 2, 3, 4, 5, 6];
+// const newNumArr = numArray.filter((num) => {
+//   return num % 2 === 0;
+// });
+
+// console.log(newNumArr);
